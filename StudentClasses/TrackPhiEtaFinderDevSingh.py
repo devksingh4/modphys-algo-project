0ad62896149f418bee9639b242a837ffda8e024f
Dev Singh <dev@devksingh.com>
2020-11-27 13:53:44 -0600

    add for neg

--- StudentClasses/TrackPhiEtaFinderDevSingh.py
+++ StudentClasses/TrackPhiEtaFinderDevSingh.py
@@ -11,13 +11,10 @@ def angle_between(vector_1, vector_2):
     return np.arccos(dot_product)
 def calc_mse(real, pred):
     return np.square(np.subtract(real,pred)).mean() 
-def plot_3d_data(data):
-    x_data = np.array(data).T[0]
-    y_data = np.array(data).T[1]
-    z_data = np.array(data).T[2]
+def plot_3d_data(x,y,z):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
-    ax.scatter(x_data, y_data, z_data)
+    ax.scatter(x, y, z)
     plt.title('All detector data', fontsize=12)
     plt.show()
 class TrackPhiEtaFinderDevSingh():
@@ -45,16 +42,14 @@ class TrackPhiEtaFinderDevSingh():
             phi = angle_between(np.array([v1[0], v1[1]]), np.array([1,0]))
             phis.append(phi)    
         phi = phis[0]
-        y_data = list(y_data)
-        plt.plot(x_data, y_data)
-        plt.show()
         if (np.average(x_data[0]) < 0  and np.average(y_data) < 0): # account for range of arccos() being from 0 to pi and not 2 pi
             phi += np.pi
         if (np.average(x_data) > 0 and np.average(y_data) < 0 and phi != 0):
             phi = -1 * phi
+        if phi < 0:
+            phi += 2 * np.pi
         while eta > np.pi:
             eta = eta - (np.pi)
         # Just return them, like this:
-        # plot_3d_data(data)
         return phi, eta
 
