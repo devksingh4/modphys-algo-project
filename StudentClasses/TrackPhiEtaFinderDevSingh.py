2b67d3f6d106262783100b9a74ded9144a31444a
Dev Singh <dev@devksingh.com>
2020-11-29 17:33:10 -0600

    prepare for submission

--- StudentClasses/TrackPhiEtaFinderDevSingh.py
+++ StudentClasses/TrackPhiEtaFinderDevSingh.py
@@ -1,11 +1,9 @@
 from Track import Track
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import pyplot as plt, cm, colors
 import math
 
 def angle_between(vector_1, vector_2):
+    """Get the angle between 2 vectors"""
     unit_vector_1 = vector_1 / np.linalg.norm(vector_1)
     unit_vector_2 = vector_2 / np.linalg.norm(vector_2)
     dot_product = np.dot(unit_vector_1, unit_vector_2)
@@ -14,45 +12,53 @@ def angle_between(vector_1, vector_2):
         return 0 
     else: 
         return angle
+
 def calc_mse(real, pred):
+    """Calculate the Mean Squared Error"""
     return np.square(np.subtract(real,pred)).mean() 
-# def plot_3d_data(x,y,z):
-#     fig = plt.figure()
-#     ax = fig.add_subplot(projection='3d')
-#     ax.scatter(x, y, z)
-#     plt.title('All detector data', fontsize=12)
-#     plt.show()
+
 class TrackPhiEtaFinderDevSingh():
-    
+
     def find_phi_eta(self, track):
-        # The track variable is of type Track
-        # Of the Track variables, only points and vertex will be filled at this point
+        
+        # Get data into numpy format
         vertex = np.array(track.vertex)
-        data = list(map(lambda point: np.array([point.x, point.y, point.z]), track.points))
-        x_data = np.array(data).T[0]
-        y_data = np.array(data).T[1]
+        data = np.array(list(map(lambda point: np.array([point.x, point.y, point.z]), track.points)))
+        x_data = data.T[0]
+        y_data = data.T[1]
+
         # Calculate etas
         thetas = []
-        for v1 in data:
-            # vector in form <x, y, z>, find angle to z-axis
-            theta = angle_between(np.array([v1[0], v1[1], v1[2] - vertex]), np.array([0,0,1]))
+        for vec in data:
+            # vector in form <x, y, z>, find angle to z-axis and adjust for vertex
+            theta = angle_between(np.array([vec[0], vec[1], vec[2] - vertex]), np.array([0,0,1]))
             thetas.append(theta)
         thetas = np.array(thetas)
+
+        # Calculate phis
         phis = []
-        for v1 in data:
+        for vec in data:
             # vector in form <x, y>, find angle to x-axis
-            phi = angle_between(np.array([v1[0], v1[1]]), np.array([1,0]))
+            phi = angle_between(np.array([vec[0], vec[1]]), np.array([1,0]))
             phis.append(phi)  
-        phi = phis[0] # np.interp(vertex, z_data, phis) # linear interpolation to vertex (doesn't actually seem to bring much benefit)
-        theta_pred = np.array(thetas).mean() # np.interp(vertex, z_data, thetas) (doesn't actually seem to bring much benefit)
-        # Not using the interpolations because it adds uncertainty if the vertex is too far away, and it doesn't bring much benefit
-        eta = -1 * np.log(np.tan(theta_pred/2))
-        if (np.average(x_data[0]) < 0  and np.average(y_data) < 0): # account for range of arccos() being from 0 to pi and not 2 pi
+        
+        # Choose best measurement to report
+        phi = phis[0]
+        theta = np.array(thetas).mean() # I don't know why but theta is consistently more accurate when using mean rather than first element
+        # Not using linear interpolations because it adds uncertainty if the vertex 
+        # is too far away from known values, and it doesn't bring much benefit to error
+
+        # Calculate eta using formula
+        eta = -1 * np.log(np.tan(theta/2))
+
+        # Account for range of arccos() not including Quadrants 3 and 4
+        # Replace negative values with their positive equivalents
+        if (np.average(x_data[0]) < 0  and np.average(y_data) < 0 and phi != 0): 
             phi += np.pi
         if (np.average(x_data) > 0 and np.average(y_data) < 0 and phi != 0):
             phi = -1 * phi
-        if phi < 0:
+        while phi < 0:
             phi += 2 * np.pi
-        # Just return them, like this:
+
         return phi, eta
 
